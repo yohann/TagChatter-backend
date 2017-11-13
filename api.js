6c592f1683c45065a7af8ae3e3ea1899dbd5ed69
yohncandido <canyoh7@gmail.com>
2017-11-13 18:13:40 -0200

    Adds POST /channels/:channelId/messages

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/users', function (req, res) {
     //reading users.json
@@ -8,7 +10,7 @@ app.get('/users', function (req, res) {
     //users to be returned
     var retUsers = [];
     
-    for (var i = 0, len = users.length; i < len; i++) {
+    for (let i = 0, len = users.length; i < len; i++) {
         //tranforming into the Users model
         retUsers.push({id: users[i].id,
                      name: users[i].first_name +" "+ users[i].last_name,
@@ -17,12 +19,10 @@ app.get('/users', function (req, res) {
 
     //function to sort by name
     function compare(a,b) {
-        if (a.name < b.name)
-            return -1;
-        if (a.name > b.name)
-            return 1;
-        return 0;
-        }
+        if(a.name==b.name)
+           return 0;
+        return a.name>b.name?1:-1
+    }
 
     //returning users sorted by name
     res.send(retUsers.sort(compare));
@@ -34,20 +34,30 @@ app.get('/channels', function (req, res) {
     
     //function to sort by name
     function compare(a,b) {
-        if (a.name < b.name)
-            return -1;
-        if (a.name > b.name)
-            return 1;
-        return 0;
-        }
+        if(a.name==b.name)
+          return 0;
+        return a.name>b.name?1:-1
+    }
 
     //deleting channels.public field
-    for (var i = 0, len = channels.length; i < len; i++) {
+    for (let i = 0, len = channels.length; i < len; i++) {
         delete channels[i].public;
     }
 
     //returning channels sorted by name
     res.send(channels.sort(compare));
+});
+
+app.post('/channels/:channelId/messages', function(req, res) {
+    var channelId = req.params.channelId;
+    var message = req.body.message;
+
+    var users = require('./users.json');
+    var sender = users.filter(function( obj ) {
+        return obj.id == req.body.author_id;
+    });
+
+    res.send({id:"idunico", content:message+"message", created_at:"22/10/2000", author:sender});
 });
 
 var server = app.listen(5000, function () {
